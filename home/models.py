cbbb3cf070acfe0b57d4bac1b8cba18e1f0dc108
Akash Kotnala <akashkotnala07@gmail.com>
2020-08-08 04:10:03 +0530

    initial commit

---
+++ home/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+# Database ----> Excel workbook
+# Models In Django ----> Table  --------> Sheet
+
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=13)
+    email = models.CharField(max_length=100)
+    content = models.TextField()
+    timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return 'Message from ' + self.name + ' - ' + self.email
+    
